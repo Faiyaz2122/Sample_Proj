61699571d698025a66a70fbc8ef830df70b4749b
Faiyaz2122 <126043440+Faiyaz2122@users.noreply.github.com>
2025-09-18 18:46:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Sample_Project.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Oracle EF Core
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseOracle(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Employees}/{action=Index}/{id?}");
+
+app.Run();
